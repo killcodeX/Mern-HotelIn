2944458dd0fed298405bc328e353729ce84e0913
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-07-18 21:56:01 +0530

    created client and server folder

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Switch, Route} from 'react-router-dom';
+import Home from './containers/Home'
+
+function App() {
+  return (
+    <Switch>
+      <Route path='/' exact component={Home} />
+    </Switch>
+  );
+}
+
+export default App;
