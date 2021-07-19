1ed76f83cc5700936b8a3465b8583a67e1ebf40e
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-07-19 10:43:38 +0530

    footer done

--- client/src/components/UI/Heading/headingStyle.js
+++ client/src/components/UI/Heading/headingStyle.js
@@ -4,6 +4,9 @@ import styled from "styled-components";
 
 export const HeadingWrapper = styled.h2`
     color:#2c2c2c;
-    font-size:20px;
+    font-size:30px;
     font-weight:700;
+    @media(max-width:480px){
+        font-size:18px;
+    }
 `
