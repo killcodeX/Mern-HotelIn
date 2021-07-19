576e501e28bf7cf7a4c0e062032a0254d90a4537
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-07-19 23:24:35 +0530

    updated single page

--- client/src/containers/SinglePage/index.js
+++ client/src/containers/SinglePage/index.js
@@ -1,13 +1,78 @@
-import React from 'react';
-import { SectionWrapper } from './singleStyle';
-import { data } from './fakeData';
+import React from "react";
+import { Heading } from "../../components/UI/Heading";
+import {
+  SectionWrapper,
+  ImageWrapper,
+  HotelImage,
+  DescWrapper,
+  HeadText,
+} from "./singleStyle";
+import {
+  FaBed,
+  FaDelicious,
+  FaUmbrellaBeach,
+  FaTv,
+  FaUserAlt,
+} from "react-icons/fa";
+import { data } from "./fakeData";
 
 export const SingleHotel = () => {
-    console.log(data)
-    return (
-        <SectionWrapper>
-            This is Single hotel
-            <img src={data.image} alt={data.name}/>
-        </SectionWrapper>
-    )
-}
+    const iconStyle = {
+        color:'#008489',
+        fontSize:'20px',
+        paddingRight:'5px'
+    }
+  console.log(data);
+  return (
+    <SectionWrapper>
+      <ImageWrapper>
+        <HotelImage src={data.image} alt={data.name} />
+      </ImageWrapper>
+      <div className="container">
+        <div className="row mt-5">
+          <div className="col-md-7 col-sm-12">
+            <Heading content={data.name} />
+            <DescWrapper>{data.description}</DescWrapper>
+            <HeadText>Details</HeadText>
+            <table class="table table-bordered">
+              <tbody>
+                <tr>
+                  <th scope="row">
+                    <FaUserAlt style={iconStyle} /> Adults
+                  </th>
+                  <td>{data.details.adults}</td>
+                </tr>
+                <tr>
+                  <th scope="row">
+                    <FaTv style={iconStyle} /> Amenities
+                  </th>
+                  <td>{data.details.ameties}</td>
+                </tr>
+                <tr>
+                  <th scope="row">
+                    <FaUmbrellaBeach style={iconStyle} /> View
+                  </th>
+                  <td>{data.details.view}</td>
+                </tr>
+                <tr>
+                  <th scope="row">
+                    <FaDelicious style={iconStyle} /> Size
+                  </th>
+                  <td>{data.details.size}</td>
+                </tr>
+                <tr>
+                  <th scope="row">
+                    <FaBed style={iconStyle} /> Bed Type
+                  </th>
+                  <td>{data.details.bedType}</td>
+                </tr>
+              </tbody>
+            </table>
+            <HeadText>Reviews</HeadText>
+          </div>
+          <div className="col-md-5 col-sm-12">2</div>
+        </div>
+      </div>
+    </SectionWrapper>
+  );
+};
--- client/src/containers/SinglePage/singleStyle.js
+++ client/src/containers/SinglePage/singleStyle.js
@@ -1,7 +1,30 @@
 import styled from "styled-components";
 
-
 export const SectionWrapper = styled.section`
-    padding: 52px 0;
+  padding: 52px 0;
+`;
 
+export const ImageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const HotelImage = styled.img`
+  width: 60%;
+  box-shadow: 0 13px 43px rgb(37 46 89 / 23%);
+`;
+
+export const DescWrapper = styled.p`
+  color: #5f6060;
+  font-family: Lato;
+  font-size: 16px;
+  font-size: 1rem;
+  line-height: 1.75;
+  margin-top:20px;
+`;
+
+export const HeadText = styled.h4`
+  font-family: "Noto Serif", serif;
+  color: #222;
+  margin-top:20px;
 `;
