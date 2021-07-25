4a5cde795050ce197a06317dde4e1eed070fd18f
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-07-26 00:04:02 +0530

    hyderabad data is updated

--- client/src/containers/Hotels/fakeData.js
+++ client/src/containers/Hotels/fakeData.js
@@ -1,56 +1,519 @@
 export const data = [
-    {
-        id:1,
-        name:'Hotel SR Grand',
-        city:'Hyderabad',
-        price:'8229',
-        rating:'4.8',
-        image:'https://uploads-bkt.s3.ap-south-1.amazonaws.com/images/1603290425979-SR7.jpg',
-        totalReview:'27'
+  {
+    id: 1,
+    name: "Novotel Hyderabad Convention Centre",
+    image:"https://cdn1.goibibo.com/voy_ing/t_g/91c8c81cf6b311e7b1850a4cef95d023.jpg",
+    description:
+      "Classic Double Rooms come with either double or single beds. \n Designed in open-concept living area, they have lots of in-room facilities.The room sizes vary from 20 to 25 sqm. \n They are also equipped with a fully-stocked minibar and snacks, air-conditioning unit, two comfortable chairs, makeup mirror, huge wardrobe, a soft hand-made carpet and ensuite bathroom.Perfect choice for honeymooners and couples.",
+    city: "Hyderabad",
+    price: "3500",
+    rating: "4.8",
+    totalReview: "27",
+    details: {
+      adults: 2,
+      ameties:
+        "air-conditioning, free wi-fi, hairdryer, in-room safety, laundry, minibar, telephone",
+      view: "beach",
+      size: "299 sq.ft.",
+      bedType: "Twin Bed sized bed",
     },
-    {
-        id:2,
-        name:'Hotel Samrat Ashok',
-        city:'Delhi',
-        price:'5929',
-        rating:'4.5',
-        image:'https://uploads-bkt.s3.ap-south-1.amazonaws.com/images/1602775364463-ash23.jpg',
-        totalReview:'3059'
-    },
-    {
-        id:3,
-        name:'Hotel Green Orchid',
-        city:'Bangalore',
-        price:'1020',
-        rating:'4.7',
-        image:'https://uploads-bkt.s3.ap-south-1.amazonaws.com/images/1602751842126-4bed53527dec11e7aef20a4cef95d023.jpg',
-        totalReview:'20'
-    },
-    {
-        id:4,
-        name:'Hotel Ocean View',
-        city:'Mumbai',
-        price:'6229',
-        rating:'4.2',
-        image:'https://uploads-bkt.s3.ap-south-1.amazonaws.com/images/1602747368687-sha1.jpg',
-        totalReview:'59'
-    },
-    {
-        id:5,
-        name:'Krishna Grand Inn',
-        city:'Chennai',
-        price:'2229',
-        rating:'4.5',
-        image:'https://uploads-bkt.s3.ap-south-1.amazonaws.com/images/1603295342262-IMG_20181025_185429.jpg',
-        totalReview:'76'
-    },
-    {
-        id:6,
-        name:'Hotel Woodland',
-        city:'Bangalore',
-        price:'5834',
-        rating:'4.1',
-        image:'https://uploads-bkt.s3.ap-south-1.amazonaws.com/images/1602778424658-getlstd-property-photo.jpg',
-        totalReview:'89'
-    }
-]
+    reviews: [
+      {
+        id: 1,
+        name: "LUCIO ROSSI",
+        image:
+          "https://secure.gravatar.com/avatar/17779da5a0bfeecb31efb4877ce083c2?s=64&r=g",
+        rating: "4.1",
+        review:
+          "Room is very neat and snug. Our window view was a court yard that disappointed a bit.",
+      },
+      {
+        id: 2,
+        name: "ERIK MAURER",
+        image:
+          "https://secure.gravatar.com/avatar/40a02bf881b0d1e982aee0e5d169575e?s=64&r=g",
+        rating: "4.5",
+        review: "Super up-to-date design and responsive stuff!",
+      },
+      {
+        id: 3,
+        name: "JOSHUA ROBERTS",
+        image:
+          "https://secure.gravatar.com/avatar/8cd09fe8c983a2c8a8c8818129950a76?s=64&r=g",
+        rating: "4.5",
+        review: "Very neat and nice room with beautiful view.",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Fairfield by Marriott Hyderabad Gachibowli",
+    image:
+      "https://cdn1.goibibo.com/voy_mmt/t_g/htl-imgs/201903271431398780-56725894c3ff11e9946f0242ac110002.jpg",
+    description:
+      "Classic Double Rooms come with either double or single beds. \n Designed in open-concept living area, they have lots of in-room facilities.The room sizes vary from 20 to 25 sqm. \n They are also equipped with a fully-stocked minibar and snacks, air-conditioning unit, two comfortable chairs, makeup mirror, huge wardrobe, a soft hand-made carpet and ensuite bathroom.Perfect choice for honeymooners and couples.",
+    city: "Hyderabad",
+    price: "2106",
+    rating: "4.5",
+    totalReview: "3059",
+    details: {
+        adults: 2,
+        ameties:
+          "air-conditioning, free wi-fi, hairdryer, in-room safety, laundry, minibar, telephone",
+        view: "beach",
+        size: "25m2",
+        bedType: "1 Queen bed",
+      },
+      reviews: [
+        {
+          id: 1,
+          name: "LUCIO ROSSI",
+          image:
+            "https://secure.gravatar.com/avatar/17779da5a0bfeecb31efb4877ce083c2?s=64&r=g",
+          rating: "4.1",
+          review:
+            "Room is very neat and snug. Our window view was a court yard that disappointed a bit.",
+        },
+        {
+          id: 2,
+          name: "ERIK MAURER",
+          image:
+            "https://secure.gravatar.com/avatar/40a02bf881b0d1e982aee0e5d169575e?s=64&r=g",
+          rating: "4.5",
+          review: "Super up-to-date design and responsive stuff!",
+        },
+        {
+          id: 3,
+          name: "JOSHUA ROBERTS",
+          image:
+            "https://secure.gravatar.com/avatar/8cd09fe8c983a2c8a8c8818129950a76?s=64&r=g",
+          rating: "4.5",
+          review: "Very neat and nice room with beautiful view.",
+        },
+      ],
+  },
+  {
+    id: 3,
+    name: "Ellaa Hotel Gachibowli",
+    image:"https://cdn1.goibibo.com/voy_ing/t_g/fab0ab8af48b11e7ab060a4cef95d023.jpg",
+    description:
+      "Classic Double Rooms come with either double or single beds. \n Designed in open-concept living area, they have lots of in-room facilities.The room sizes vary from 20 to 25 sqm. \n They are also equipped with a fully-stocked minibar and snacks, air-conditioning unit, two comfortable chairs, makeup mirror, huge wardrobe, a soft hand-made carpet and ensuite bathroom.Perfect choice for honeymooners and couples.",
+    city: "Hyderabad",
+    price: "1926",
+    rating: "4.5",
+    totalReview: "3059",
+    details: {
+        adults: 2,
+        ameties:
+          "air-conditioning, free wi-fi, hairdryer, in-room safety, laundry, minibar, telephone",
+        view: "beach",
+        size: "25m2",
+        bedType: "1 Queen bed",
+      },
+      reviews: [
+        {
+          id: 1,
+          name: "LUCIO ROSSI",
+          image:
+            "https://secure.gravatar.com/avatar/17779da5a0bfeecb31efb4877ce083c2?s=64&r=g",
+          rating: "4.1",
+          review:
+            "Room is very neat and snug. Our window view was a court yard that disappointed a bit.",
+        },
+        {
+          id: 2,
+          name: "ERIK MAURER",
+          image:
+            "https://secure.gravatar.com/avatar/40a02bf881b0d1e982aee0e5d169575e?s=64&r=g",
+          rating: "4.5",
+          review: "Super up-to-date design and responsive stuff!",
+        },
+        {
+          id: 3,
+          name: "JOSHUA ROBERTS",
+          image:
+            "https://secure.gravatar.com/avatar/8cd09fe8c983a2c8a8c8818129950a76?s=64&r=g",
+          rating: "4.5",
+          review: "Very neat and nice room with beautiful view.",
+        },
+      ],
+  },
+  {
+    id: 4,
+    name: "Hotel Imperial Classic",
+    image:"https://cdn1.goibibo.com/voy_mmt/t_g/htl-imgs/201108301434398753-b09bd896f90c11e7b48f0a9df65c8753.jpg",
+    description:
+      "Hotel Imperial Classic is a great choice for travellers looking for a 3 star hotel in Hyderabad. It is located in Himayat Nagar.This Hotel stands out as one of the highly recommended hotel in Hyderabad and is recommended by 87% of our guests. Hotel is rated 4.1 out of 5, which is considered as very good. ",
+    city: "Hyderabad",
+    price: "952",
+    rating: "4.5",
+    totalReview: "3059",
+    details: {
+        adults: 2,
+        ameties:
+          "air-conditioning, free wi-fi, hairdryer, in-room safety, laundry, minibar, telephone",
+        view: "Pool",
+        size: "180 sq.ft.",
+        bedType: "Double Bed sized bed",
+      },
+      reviews: [
+        {
+          id: 1,
+          name: "LUCIO ROSSI",
+          image:
+            "https://secure.gravatar.com/avatar/17779da5a0bfeecb31efb4877ce083c2?s=64&r=g",
+          rating: "4.1",
+          review:
+            "Room is very neat and snug. Our window view was a court yard that disappointed a bit.",
+        },
+        {
+          id: 2,
+          name: "ERIK MAURER",
+          image:
+            "https://secure.gravatar.com/avatar/40a02bf881b0d1e982aee0e5d169575e?s=64&r=g",
+          rating: "4.5",
+          review: "Super up-to-date design and responsive stuff!",
+        },
+        {
+          id: 3,
+          name: "JOSHUA ROBERTS",
+          image:
+            "https://secure.gravatar.com/avatar/8cd09fe8c983a2c8a8c8818129950a76?s=64&r=g",
+          rating: "4.5",
+          review: "Very neat and nice room with beautiful view.",
+        },
+      ],
+  },
+  {
+    id: 5,
+    name: "Radisson Hyderabad Hitec City, India",
+    image:"https://cdn1.goibibo.com/voy_ing/t_g/32d4c786780811e7ad04025f77df004f.jpg",
+    description:
+      "Radisson Hyderabad Hitec City, India is a great choice for travellers looking for a 5 star hotel in Hyderabad. It is located in Gachibowli.This Hotel stands out as one of the highly recommended hotel in Hyderabad and is recommended by 91% of our guests. Hotel is rated 4.2 out of 5, which is considered as very good. ",
+    city: "Hyderabad",
+    price: "2290",
+    rating: "4.2",
+    totalReview: "2780",
+    details: {
+        adults: 2,
+        ameties:
+          "air-conditioning, free wi-fi, hairdryer, in-room safety, laundry, minibar, telephone",
+        view: "Malls",
+        size: "364 sq.ft.",
+        bedType: "1 King bed or 2 Twin Bed(s) sized bed",
+      },
+      reviews: [
+        {
+          id: 1,
+          name: "LUCIO ROSSI",
+          image:
+            "https://secure.gravatar.com/avatar/17779da5a0bfeecb31efb4877ce083c2?s=64&r=g",
+          rating: "4.1",
+          review:
+            "Room is very neat and snug. Our window view was a court yard that disappointed a bit.",
+        },
+        {
+          id: 2,
+          name: "ERIK MAURER",
+          image:
+            "https://secure.gravatar.com/avatar/40a02bf881b0d1e982aee0e5d169575e?s=64&r=g",
+          rating: "4.5",
+          review: "Super up-to-date design and responsive stuff!",
+        },
+        {
+          id: 3,
+          name: "JOSHUA ROBERTS",
+          image:
+            "https://secure.gravatar.com/avatar/8cd09fe8c983a2c8a8c8818129950a76?s=64&r=g",
+          rating: "4.5",
+          review: "Very neat and nice room with beautiful view.",
+        },
+      ],
+  },
+  {
+    id: 6,
+    name: "Marriott Executive Apartments Hyderabad",
+    image:"https://cdn1.goibibo.com/voy_mmt/t_g/htl-imgs/201801031617525650-208dac22109811e890420a9df65c8753.jpg",
+    description:
+      "Marriott Executive Apartments Hyderabad is a great choice for travellers looking for a 5 star hotel in Hyderabad. It is located in Kondapur.This Hotel stands out as one of the highly recommended hotel in Hyderabad and is recommended by 88% of our guests. Hotel is rated 4.2 out of 5, which is considered as very good. ",
+    city: "Hyderabad",
+    price: "5750",
+    rating: "4.2",
+    totalReview: "85",
+    details: {
+        adults: 2,
+        ameties:
+          "air-conditioning, free wi-fi, hairdryer, in-room safety, laundry, minibar, telephone",
+        view: "Pools",
+        size: "900 sq.ft.",
+        bedType: "King Bed sized bed",
+      },
+      reviews: [
+        {
+          id: 1,
+          name: "LUCIO ROSSI",
+          image:
+            "https://secure.gravatar.com/avatar/17779da5a0bfeecb31efb4877ce083c2?s=64&r=g",
+          rating: "4.1",
+          review:
+            "Room is very neat and snug. Our window view was a court yard that disappointed a bit.",
+        },
+        {
+          id: 2,
+          name: "ERIK MAURER",
+          image:
+            "https://secure.gravatar.com/avatar/40a02bf881b0d1e982aee0e5d169575e?s=64&r=g",
+          rating: "4.5",
+          review: "Super up-to-date design and responsive stuff!",
+        },
+        {
+          id: 3,
+          name: "JOSHUA ROBERTS",
+          image:
+            "https://secure.gravatar.com/avatar/8cd09fe8c983a2c8a8c8818129950a76?s=64&r=g",
+          rating: "4.5",
+          review: "Very neat and nice room with beautiful view.",
+        },
+      ],
+  },
+//   this section for mumbai hotels
+  {
+    id: 7,
+    name: "Radisson Blu Mumbai International Airport",
+    image:"https://cdn1.goibibo.com/voy_ing/t_g/6a653fe4d7ac11eabeec0242ac110005.jpg",
+    city: "Mumbai",
+    price: "8229",
+    rating: "4.8",
+    totalReview: "27",
+  },
+  {
+    id: 8,
+    name: "Hotel Samrat Ashok",
+    city: "Delhi",
+    price: "5929",
+    rating: "4.5",
+    image:
+      "https://uploads-bkt.s3.ap-south-1.amazonaws.com/images/1602775364463-ash23.jpg",
+    totalReview: "3059",
+  },
+  {
+    id: 9,
+    name: "Hotel Green Orchid",
+    city: "Bangalore",
+    price: "1020",
+    rating: "4.7",
+    image:
+      "https://uploads-bkt.s3.ap-south-1.amazonaws.com/images/1602751842126-4bed53527dec11e7aef20a4cef95d023.jpg",
+    totalReview: "20",
+  },
+  {
+    id: 10,
+    name: "Hotel Ocean View",
+    city: "Mumbai",
+    price: "6229",
+    rating: "4.2",
+    image:
+      "https://uploads-bkt.s3.ap-south-1.amazonaws.com/images/1602747368687-sha1.jpg",
+    totalReview: "59",
+  },
+  {
+    id: 11,
+    name: "Krishna Grand Inn",
+    city: "Chennai",
+    price: "2229",
+    rating: "4.5",
+    image:
+      "https://uploads-bkt.s3.ap-south-1.amazonaws.com/images/1603295342262-IMG_20181025_185429.jpg",
+    totalReview: "76",
+  },
+  {
+    id: 12,
+    name: "Hotel SR Grand",
+    city: "Hyderabad",
+    price: "8229",
+    rating: "4.8",
+    image:
+      "https://uploads-bkt.s3.ap-south-1.amazonaws.com/images/1603290425979-SR7.jpg",
+    totalReview: "27",
+  },
+  {
+    id: 13,
+    name: "Hotel Samrat Ashok",
+    city: "Delhi",
+    price: "5929",
+    rating: "4.5",
+    image:
+      "https://uploads-bkt.s3.ap-south-1.amazonaws.com/images/1602775364463-ash23.jpg",
+    totalReview: "3059",
+  },
+  {
+    id: 14,
+    name: "Hotel Green Orchid",
+    city: "Bangalore",
+    price: "1020",
+    rating: "4.7",
+    image:
+      "https://uploads-bkt.s3.ap-south-1.amazonaws.com/images/1602751842126-4bed53527dec11e7aef20a4cef95d023.jpg",
+    totalReview: "20",
+  },
+  {
+    id: 15,
+    name: "Hotel Ocean View",
+    city: "Mumbai",
+    price: "6229",
+    rating: "4.2",
+    image:
+      "https://uploads-bkt.s3.ap-south-1.amazonaws.com/images/1602747368687-sha1.jpg",
+    totalReview: "59",
+  },
+  {
+    id: 16,
+    name: "Krishna Grand Inn",
+    city: "Chennai",
+    price: "2229",
+    rating: "4.5",
+    image:
+      "https://uploads-bkt.s3.ap-south-1.amazonaws.com/images/1603295342262-IMG_20181025_185429.jpg",
+    totalReview: "76",
+  },
+  {
+    id: 17,
+    name: "Hotel SR Grand",
+    city: "Hyderabad",
+    price: "8229",
+    rating: "4.8",
+    image:
+      "https://uploads-bkt.s3.ap-south-1.amazonaws.com/images/1603290425979-SR7.jpg",
+    totalReview: "27",
+  },
+  {
+    id: 18,
+    name: "Hotel Samrat Ashok",
+    city: "Delhi",
+    price: "5929",
+    rating: "4.5",
+    image:
+      "https://uploads-bkt.s3.ap-south-1.amazonaws.com/images/1602775364463-ash23.jpg",
+    totalReview: "3059",
+  },
+  {
+    id: 19,
+    name: "Hotel Green Orchid",
+    city: "Bangalore",
+    price: "1020",
+    rating: "4.7",
+    image:
+      "https://uploads-bkt.s3.ap-south-1.amazonaws.com/images/1602751842126-4bed53527dec11e7aef20a4cef95d023.jpg",
+    totalReview: "20",
+  },
+  {
+    id: 20,
+    name: "Hotel Ocean View",
+    city: "Mumbai",
+    price: "6229",
+    rating: "4.2",
+    image:
+      "https://uploads-bkt.s3.ap-south-1.amazonaws.com/images/1602747368687-sha1.jpg",
+    totalReview: "59",
+  },
+  {
+    id: 21,
+    name: "Krishna Grand Inn",
+    city: "Chennai",
+    price: "2229",
+    rating: "4.5",
+    image:
+      "https://uploads-bkt.s3.ap-south-1.amazonaws.com/images/1603295342262-IMG_20181025_185429.jpg",
+    totalReview: "76",
+  },
+  {
+    id: 22,
+    name: "Hotel SR Grand",
+    city: "Hyderabad",
+    price: "8229",
+    rating: "4.8",
+    image:
+      "https://uploads-bkt.s3.ap-south-1.amazonaws.com/images/1603290425979-SR7.jpg",
+    totalReview: "27",
+  },
+  {
+    id: 23,
+    name: "Hotel Samrat Ashok",
+    city: "Delhi",
+    price: "5929",
+    rating: "4.5",
+    image:
+      "https://uploads-bkt.s3.ap-south-1.amazonaws.com/images/1602775364463-ash23.jpg",
+    totalReview: "3059",
+  },
+  {
+    id: 24,
+    name: "Hotel Green Orchid",
+    city: "Bangalore",
+    price: "1020",
+    rating: "4.7",
+    image:
+      "https://uploads-bkt.s3.ap-south-1.amazonaws.com/images/1602751842126-4bed53527dec11e7aef20a4cef95d023.jpg",
+    totalReview: "20",
+  },
+  {
+    id: 25,
+    name: "Hotel Ocean View",
+    city: "Mumbai",
+    price: "6229",
+    rating: "4.2",
+    image:
+      "https://uploads-bkt.s3.ap-south-1.amazonaws.com/images/1602747368687-sha1.jpg",
+    totalReview: "59",
+  },
+  {
+    id: 26,
+    name: "Krishna Grand Inn",
+    city: "Chennai",
+    price: "2229",
+    rating: "4.5",
+    image:
+      "https://uploads-bkt.s3.ap-south-1.amazonaws.com/images/1603295342262-IMG_20181025_185429.jpg",
+    totalReview: "76",
+  },
+  {
+    id: 27,
+    name: "Hotel SR Grand",
+    city: "Hyderabad",
+    price: "8229",
+    rating: "4.8",
+    image:
+      "https://uploads-bkt.s3.ap-south-1.amazonaws.com/images/1603290425979-SR7.jpg",
+    totalReview: "27",
+  },
+  {
+    id: 28,
+    name: "Hotel Samrat Ashok",
+    city: "Delhi",
+    price: "5929",
+    rating: "4.5",
+    image:
+      "https://uploads-bkt.s3.ap-south-1.amazonaws.com/images/1602775364463-ash23.jpg",
+    totalReview: "3059",
+  },
+  {
+    id: 29,
+    name: "Hotel Green Orchid",
+    city: "Bangalore",
+    price: "1020",
+    rating: "4.7",
+    image:
+      "https://uploads-bkt.s3.ap-south-1.amazonaws.com/images/1602751842126-4bed53527dec11e7aef20a4cef95d023.jpg",
+    totalReview: "20",
+  },
+  {
+    id: 30,
+    name: "Hotel Ocean View",
+    city: "Mumbai",
+    price: "6229",
+    rating: "4.2",
+    image:
+      "https://uploads-bkt.s3.ap-south-1.amazonaws.com/images/1602747368687-sha1.jpg",
+    totalReview: "59",
+  },
+];
