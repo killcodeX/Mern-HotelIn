6d015d5c75c4490d6c34f77b55871d3643daa30b
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-08-29 18:06:48 +0530

    created auth reducers

--- client/src/redux/actions/actionConstant.js
+++ client/src/redux/actions/actionConstant.js
@@ -1,3 +1,11 @@
+// for users
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
+export const SIGNUP_SUCCESS = "SIGNUP_SUCCESS";
+export const VERIFY_SUCCESS = "VERIFY_SUCCESS";
+export const VERIFY_LOCAL_STORAGE = "VERIFY_LOCAL_STORAGE";
+
+
 // for hotel
 export const GetAllHotel = 'GET_ALL_HotelS';
 export const GetSingleHotel = 'GET_SINGLE_HOTEL';
