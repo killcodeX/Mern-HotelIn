bd86ecc723619b94f675b827a1fcd0a4b79886d2
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-08-29 18:22:07 +0530

    installed required lib

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors';
-import hotelRoutes from './routes/route.js'
+import hotelRoutes from './routes/route.js';
+import dotenv from 'dotenv';
 
 const app = express()
+dotenv.config()
 
 app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
@@ -14,7 +16,7 @@ app.use(cors())
 app.use('/hotel', hotelRoutes)
 
 // for database
-const connectionUrl = "mongodb+srv://Aaquib5076:Aaquib@5076@cluster0.oknup.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const connectionUrl = `mongodb+srv://Aaquib5076:${process.env.DB_PASSWORD}@cluster0.oknup.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const PORT = process.env.PORT || 5000;
 
